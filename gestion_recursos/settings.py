1680292002f6cc3e49cbc7923d0d2f01ba1ae07f
Ceragos <antonio.casta_yo@hotmail.com>
2019-04-14 20:43:09 -0500

    Conexión Exitosa

--- gestion_recursos/settings.py
+++ gestion_recursos/settings.py
@@ -77,8 +77,14 @@ WSGI_APPLICATION = 'gestion_recursos.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sisgerecu',
+        'USER': 'ceragos36',
+        'PASSWORD': 'seforabu-7',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
