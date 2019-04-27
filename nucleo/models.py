d314186eff81541b586fb1fd90bb71dd72ba24e7
Ceragos <antonio.casta_yo@hotmail.com>
2019-04-26 23:33:30 -0500

    Se crea modelo marcado de tiempo en el modulo Nucleo y se adaptan los modelos del modulo Recursos

--- nucleo/models.py
+++ nucleo/models.py
@@ -1,3 +1,18 @@
+import datetime
+
 from django.db import models
 
 # Create your models here.
+class MarcadorTiempo(models.Model):
+    fecha_creacion = models.DateTimeField(auto_now_add=True)
+    fecha_modificacion = models.DateTimeField(auto_now_add=False, null=True, blank=True)
+
+    def save(self, *args, **kwargs):
+        if not self.pk:
+            self.fecha_creacion = datetime.datetime.today()
+        else:
+            self.fecha_modificacion = datetime.datetime.today()
+        super(MarcadorTiempo, self).save(*args, **kwargs)
+
+    class Meta:
+        abstract = True
