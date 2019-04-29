87f9e8566fefcf41b0b905c364820193bb677515
Ceragos <antonio.casta_yo@hotmail.com>
2019-04-29 01:34:15 -0500

    Galeria de imagenes para recursos fisicos y tecnologicos

--- recursos/admin.py
+++ recursos/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
-from recursos.models import RecursoFisico, RecursoTecnologico, TipoAula, TipoEquipo
+from recursos.models import RecursoFisico, RecursoTecnologico, TipoAula, TipoEquipo, GaleriaRecursoFisico, \
+    GaleriaRecursoTecnologico
+
 
 class TipoEquipoAdmin(admin.ModelAdmin):
     readonly_fields = ['fecha_creacion', 'fecha_modificacion']
@@ -17,7 +19,17 @@ class RecursoTecnologicoAdmin(admin.ModelAdmin):
     fields = ['tipo_equipo', 'numero', 'nombre', 'marca', 'referencia', 'color', 'caracteristicas', 'contenido', 'fecha_creacion', 'fecha_modificacion']
     readonly_fields = ['nombre', 'fecha_creacion', 'fecha_modificacion']
 
+class GaleriaRecursoFisicoAdmin(admin.ModelAdmin):
+    fields = ['recurso_fisico', 'imagen','fecha_creacion', 'fecha_modificacion']
+    readonly_fields = ['fecha_creacion', 'fecha_modificacion']
+
+class GaleriaRecursoTecnologicoAdmin(admin.ModelAdmin):
+    fields = ['recurso_tecnologico', 'imagen','fecha_creacion', 'fecha_modificacion']
+    readonly_fields = ['fecha_creacion', 'fecha_modificacion']
+
 admin.site.register(TipoEquipo, TipoEquipoAdmin)
 admin.site.register(TipoAula, TipoAulaAdmin)
 admin.site.register(RecursoFisico, RecursoFisicoAdmin)
 admin.site.register(RecursoTecnologico, RecursoTecnologicoAdmin)
+admin.site.register(GaleriaRecursoFisico, GaleriaRecursoFisicoAdmin)
+admin.site.register(GaleriaRecursoTecnologico, GaleriaRecursoTecnologicoAdmin)
--- recursos/models.py
+++ recursos/models.py
@@ -1,3 +1,5 @@
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 from django.db import models
 
 # Create your models here.
@@ -52,10 +54,15 @@ class RecursoFisico(MarcadorTiempo, Recurso):
     def __str__(self):
         return '%s' % self.nombre
 
-    def save(self, *args, **kwargs):
-        if not self.nombre:
-            self.nombre = '%s %s' % (self.tipo_aula, self.numero)
+    def clean(self):
+        raise ValidationError("El recurso fisico ya existe")
 
+    def save(self, *args, **kwargs):
+        try:
+            if not self.nombre:
+                self.nombre = '%s %s' % (self.tipo_aula, self.numero)
+        except:
+            self.full_clean()
         return super(RecursoFisico, self).save(*args, **kwargs)
 
 
@@ -74,8 +81,13 @@ class RecursoTecnologico(MarcadorTiempo, Recurso):
     def __str__(self):
         return '%s' % self.nombre
 
-    def save(self, *args, **kwargs):
-        if not self.nombre:
-            self.nombre = '%s %s' % (self.tipo_equipo, self.numero)
+    def clean(self):
+        raise ValidationError("El recurso tecnologico ya existe")
 
+    def save(self, *args, **kwargs):
+        try:
+            if not self.nombre:
+                self.nombre = '%s %s' % (self.tipo_equipo, self.numero)
+        except:
+            self.full_clean()
         return super(RecursoTecnologico, self).save(*args, **kwargs)
