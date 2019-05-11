7aa0b4d5a614b7257cb47b2976d7ab896b18a741
Ceragos <antonio.casta_yo@hotmail.com>
2019-05-11 11:03:17 -0500

    Modificación del placeholder de usuario y contraseña

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -5,5 +5,5 @@ class SisgerecuAuthenticationForm(AuthenticationForm):
 
     def __init__(self, *args, **kwargs):
         super(SisgerecuAuthenticationForm, self).__init__(*args, **kwargs)
-        self.fields["username"].widget.attrs.update({'class': 'username', 'placeholder': 'username'})
-        self.fields['password'].widget.attrs.update({'class': 'password', 'placeholder': 'password'})
+        self.fields["username"].widget.attrs.update({'class': 'username', 'placeholder': 'Usuario'})
+        self.fields['password'].widget.attrs.update({'class': 'password', 'placeholder': 'Contraseña'})
