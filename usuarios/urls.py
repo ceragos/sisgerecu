cf33f44cac57c968718a417ed1088333d0f354cb
Ceragos <antonio.casta_yo@hotmail.com>
2019-05-11 00:11:26 -0500

    Configuración logout

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from usuarios.views import SisgerecuLoginView
+from usuarios.views import SisgerecuLoginView, SisgerecuLogoutView
 
 urlpatterns = [
     path(r'login/', SisgerecuLoginView.as_view(), name='login'),
+    path(r'logout/', SisgerecuLogoutView.as_view(), name="logout")
 ]
--- usuarios/views.py
+++ usuarios/views.py
@@ -1,8 +1,11 @@
-from django.contrib.auth import login
-from django.contrib.auth.views import LoginView
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
 from django.http import HttpResponseRedirect
+
+from gestion_recursos import settings
 from usuarios.forms import SisgerecuAuthenticationForm
 from django.urls import reverse_lazy, reverse
+
 
 # Create your views here.
 
@@ -14,10 +17,18 @@ class SisgerecuLoginView(LoginView):
     def form_valid(self, form):
         user = form.get_user()
         if user is not None:
-          login(self.request, user)
-          return HttpResponseRedirect(self.get_success_url)
-
+            login(self.request, user)
+            return HttpResponseRedirect(self.get_success_url)
 
     def get_context_data(self, **kwargs):
         context = super(SisgerecuLoginView, self).get_context_data(**kwargs)
         return context
+
+class SisgerecuLogoutView(LogoutView):
+    template_name = 'usuarios/login.html'
+    form_class = SisgerecuAuthenticationForm
+    next_page = settings.LOGIN_URL
+
+    def get(self, *args, **kwargs):
+        logout(self.request)
+        return super(SisgerecuLogoutView, self).get(*args, **kwargs)
