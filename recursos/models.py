62fc68e5d98d8733b399dec85e625d327ef1be49
ceragos <jesus.a.castaneda.v@gmail.com>
2019-07-02 20:37:03 -0500

    Corrección recursos

--- recursos/models.py
+++ recursos/models.py
@@ -54,15 +54,12 @@ class RecursoFisico(MarcadorTiempo, Recurso):
     def __str__(self):
         return '%s' % self.nombre
 
-    def clean(self):
-        raise ValidationError("El recurso fisico ya existe")
-
     def save(self, *args, **kwargs):
         try:
             if not self.nombre:
                 self.nombre = '%s %s' % (self.tipo_aula, self.numero)
-        except:
-            self.full_clean()
+        except Exception as ex:
+            print(ex)
         return super(RecursoFisico, self).save(*args, **kwargs)
 
 
@@ -81,15 +78,12 @@ class RecursoTecnologico(MarcadorTiempo, Recurso):
     def __str__(self):
         return '%s' % self.nombre
 
-    def clean(self):
-        raise ValidationError("El recurso tecnologico ya existe")
-
     def save(self, *args, **kwargs):
         try:
             if not self.nombre:
                 self.nombre = '%s %s' % (self.tipo_equipo, self.numero)
-        except:
-            self.full_clean()
+        except Exception as ex:
+            print(ex)
         return super(RecursoTecnologico, self).save(*args, **kwargs)
 
 
