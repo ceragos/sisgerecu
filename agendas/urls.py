ede71a9bee6b3fb65a3c4833fb11b90eca159659
ceragos <jesus.a.castaneda.v@gmail.com>
2019-07-15 02:00:04 -0500

    Active

--- agendas/urls.py
+++ agendas/urls.py
@@ -5,7 +5,7 @@ from agendas.views import MiAgendaListView, AgendaCreateView, AgendaGeneralListV
 
 urlpatterns = [
 
-    path(r'agenda_general', login_required(AgendaGeneralListView.as_view()), name='agenda_general.listar'),
-    path(r'mi_agenda', login_required(MiAgendaListView.as_view()), name='mi_agenda.listar'),
+    path(r'agenda_general/', login_required(AgendaGeneralListView.as_view()), name='agenda_general.listar'),
+    path(r'mi_agenda/', login_required(MiAgendaListView.as_view()), name='mi_agenda.listar'),
     path(r'crear/', login_required(AgendaCreateView.as_view()), name='agendas.crear'),
 ]
