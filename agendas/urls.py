1f3426dd00c5961be75b787a999091c13bf0509b
ceragos <jesus.a.castaneda.v@gmail.com>
2019-07-15 02:07:53 -0500

    Active

--- agendas/urls.py
+++ agendas/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
 
     path(r'agenda_general/', login_required(AgendaGeneralListView.as_view()), name='agenda_general.listar'),
     path(r'mi_agenda/', login_required(MiAgendaListView.as_view()), name='mi_agenda.listar'),
-    path(r'crear/', login_required(AgendaCreateView.as_view()), name='agendas.crear'),
+    path(r'mi_agenda/crear/', login_required(AgendaCreateView.as_view()), name='agendas.crear'),
 ]
