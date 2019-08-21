4d8d91ad9c2bf43d4fc2b976837cf848227daa48
ceragos <jesus.a.castaneda.v@gmail.com>
2019-08-21 09:13:58 -0500

    Excepciones en la validacion de campos

--- agendas/forms.py
+++ agendas/forms.py
@@ -28,8 +28,9 @@ class AgendaForm(forms.ModelForm):
         }
 
     def clean_hora_devolucion(self):
-        hora_devolucion = self.cleaned_data['hora_devolucion']
+        print(self.cleaned_data)
         hora_separacion = self.cleaned_data['hora_separacion']
+        hora_devolucion = self.cleaned_data['hora_devolucion']
 
         if hora_separacion > hora_devolucion:
             raise ValidationError('Esta hora no puede ser anterior a la hora de separación')
@@ -41,54 +42,62 @@ class AgendaForm(forms.ModelForm):
 
     def clean_recurso_fisico(self):
         recurso_fisico = self.cleaned_data['recurso_fisico']
-        fecha_separacion = self.cleaned_data['fecha_separacion']
-        hora_separacion = self.cleaned_data['hora_separacion']
-        hora_devolucion = self.cleaned_data['hora_devolucion']
+        try:
+            fecha_separacion = self.cleaned_data['fecha_separacion']
+            hora_separacion = self.cleaned_data['hora_separacion']
+            hora_devolucion = self.cleaned_data['hora_devolucion']
 
-        # Consulta de los elementos existentes en el mismo horario
-        agenda_dia = Agenda.objects.filter(fecha_separacion=fecha_separacion)
-        agenda_dia = agenda_dia.filter(Q(hora_separacion__range=(hora_separacion, hora_devolucion))
-                                       | Q(hora_devolucion__range=(hora_separacion, hora_devolucion)))
+            # Consulta de los elementos existentes en el mismo horario
+            agenda_dia = Agenda.objects.filter(fecha_separacion=fecha_separacion)
+            agenda_dia = agenda_dia.filter(Q(hora_separacion__range=(hora_separacion, hora_devolucion))
+                                           | Q(hora_devolucion__range=(hora_separacion, hora_devolucion)))
 
-        # Validación realizada para excluir si es una instancia de si misma, y así queda habilitada para hacer edición
-        if self.instance:
-            agenda_dia = agenda_dia.exclude(pk=self.instance.pk)
+            # Validación realizada para excluir si es una instancia de si misma,
+            # y así queda habilitada para hacer edición
+            if self.instance:
+                agenda_dia = agenda_dia.exclude(pk=self.instance.pk)
 
-        # Validación de disponibilidad
-        for agenda in agenda_dia:
-            if recurso_fisico == agenda.recurso_fisico:
-                raise ValidationError('El {} ya fue agendado desde las {} hasta las {} por {}'.format(
-                    recurso_fisico,
-                    agenda.hora_separacion.strftime('%I:%M %p'),
-                    agenda.hora_devolucion.strftime('%I:%M %p'),
-                    agenda.usuario))
+            # Validación de disponibilidad
+            for agenda in agenda_dia:
+                if recurso_fisico == agenda.recurso_fisico:
+                    raise ValidationError('El {} ya fue agendado desde las {} hasta las {} por {}'.format(
+                        recurso_fisico,
+                        agenda.hora_separacion.strftime('%I:%M %p'),
+                        agenda.hora_devolucion.strftime('%I:%M %p'),
+                        agenda.usuario))
+        except Exception as ex:
+            print('error: ', ex)
 
         return recurso_fisico
 
     def clean_recurso_tecnologico(self):
         recurso_tecnologico = self.cleaned_data['recurso_tecnologico']
-        fecha_separacion = self.cleaned_data['fecha_separacion']
-        hora_separacion = self.cleaned_data['hora_separacion']
-        hora_devolucion = self.cleaned_data['hora_devolucion']
+        try:
+            fecha_separacion = self.cleaned_data['fecha_separacion']
+            hora_separacion = self.cleaned_data['hora_separacion']
+            hora_devolucion = self.cleaned_data['hora_devolucion']
 
-        # Consulta de los elementos existentes en el mismo horario
-        agenda_dia = Agenda.objects.filter(fecha_separacion=fecha_separacion)
-        agenda_dia = agenda_dia.filter(Q(hora_separacion__range=(hora_separacion, hora_devolucion))
-                                       | Q(hora_devolucion__range=(hora_separacion, hora_devolucion)))
+            # Consulta de los elementos existentes en el mismo horario
+            agenda_dia = Agenda.objects.filter(fecha_separacion=fecha_separacion)
+            agenda_dia = agenda_dia.filter(Q(hora_separacion__range=(hora_separacion, hora_devolucion))
+                                           | Q(hora_devolucion__range=(hora_separacion, hora_devolucion)))
 
-        # Validación realizada para excluir si es una instancia de si misma, y así queda habilitada para hacer edición
-        if self.instance:
-            agenda_dia = agenda_dia.exclude(pk=self.instance.pk)
+            # Validación realizada para excluir si es una instancia de si misma,
+            # y así queda habilitada para hacer edición
+            if self.instance:
+                agenda_dia = agenda_dia.exclude(pk=self.instance.pk)
 
-        # Validación de disponibilidad
-        for agenda in agenda_dia:
-            for recurso in recurso_tecnologico:
-                if recurso in agenda.recurso_tecnologico.all():
-                    raise ValidationError('El {} ya fue agendado desde las {} hasta las {} por {}'.format(
-                        recurso,
-                        agenda.hora_separacion.strftime('%I:%M %p'),
-                        agenda.hora_devolucion.strftime('%I:%M %p'),
-                        agenda.usuario))
+            # Validación de disponibilidad
+            for agenda in agenda_dia:
+                for recurso in recurso_tecnologico:
+                    if recurso in agenda.recurso_tecnologico.all():
+                        raise ValidationError('El {} ya fue agendado desde las {} hasta las {} por {}'.format(
+                            recurso,
+                            agenda.hora_separacion.strftime('%I:%M %p'),
+                            agenda.hora_devolucion.strftime('%I:%M %p'),
+                            agenda.usuario))
+        except Exception as ex:
+            print('error: ', ex)
 
         return recurso_tecnologico
 
