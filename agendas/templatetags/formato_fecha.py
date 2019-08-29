aa86c4f53c54c376adba1d4ef482575888c67989
ceragos <jesus.a.castaneda.v@gmail.com>
2019-08-28 23:46:13 -0500

    Validacion botones mi agenda

--- agendas/templatetags/formato_fecha.py
+++ agendas/templatetags/formato_fecha.py
@@ -14,8 +14,8 @@ def validar_botones_agenda(reserva):
     valida que la fecha de reserva no sea anterior a la actual
     :return:
     """
-    import datetime
-    fecha_hora_reserva = datetime.datetime.combine(reserva.fecha_separacion, reserva.hora_separacion)
+    from datetime import datetime
+    fecha_hora_reserva = datetime.combine(reserva.fecha_separacion, reserva.hora_separacion)
     fecha_hora_actual = datetime.now()
     if fecha_hora_reserva < fecha_hora_actual:
         return False
